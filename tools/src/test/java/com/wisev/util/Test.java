83be57c50232484c17755922b4009d84d6e9a137
gubin <scgubin@163.com>
2019-07-09 13:57:17 +0800

    commit last++

--- tools/src/test/java/com/wisev/util/Test.java
+++ tools/src/test/java/com/wisev/util/Test.java
@@ -16,14 +16,27 @@ public class Test{
 
 
 
-	public static void main(String[] args) throws Exception  {
-        System.out.println("sdg");
-        HashMap<Integer,Character> hashMap = new HashMap<>(16,2);
-        String a = "update customer_info set department_company_id = ";
-		for(int i = 0 ;i < 16;i++) {
-            hashMap.put(i,a.charAt(i));
-		}
-        System.out.println(hashMap);
+
+
+	public static void main(String[] args)   {
+        String[] strings = new String[]{"a","a","a","a","b","b","c","d"};
+        HashMap<String,Integer> hashMap = new HashMap<>();
+        List<String> list = Arrays.asList(strings);
+        for(String str : list) {
+            hashMap.put(str,hashMap.getOrDefault(str,0) + 1);
+        }
+        PriorityQueue<String> queue = new PriorityQueue<>(Comparator.comparing(hashMap::get));
+        for(String key : hashMap.keySet()) {
+            queue.offer(key);
+         /*   if(queue.size() > 2) {
+                queue.poll();
+            }*/
+        }
+        List<String> result = new ArrayList<>();
+        while(!queue.isEmpty()) {
+            result.add(queue.poll());
+        }
+        System.out.println(result);
 
 
 
